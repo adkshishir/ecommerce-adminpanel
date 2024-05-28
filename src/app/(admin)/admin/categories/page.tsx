7153fe05bc28c33415cac66cb8f99d10cbd4e1d8
adkshishir <adhikarishishir50@gmail.com>
2024-05-28 22:27:27 +0545

    add : categories

---
+++ src/app/(admin)/admin/categories/page.tsx
@@ -0,0 +1,12 @@
+import { Category } from '@/components/category'
+import React from 'react'
+
+ const page = () => {
+  return (
+      <div>
+          <Category/>
+    </div>
+  )
+ }
+
+export default page
