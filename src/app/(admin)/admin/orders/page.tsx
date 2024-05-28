3b376a53a1588a9d2773b14421aeebe65d49c55c
adkshishir <adhikarishishir50@gmail.com>
2024-05-28 22:27:53 +0545

    add : orderlist

---
+++ src/app/(admin)/admin/orders/page.tsx
@@ -0,0 +1,12 @@
+import { OrderList } from '@/components/order-list';
+import React from 'react';
+
+const page = () => {
+  return (
+    <>
+      <OrderList />
+    </>
+  );
+};
+
+export default page;
