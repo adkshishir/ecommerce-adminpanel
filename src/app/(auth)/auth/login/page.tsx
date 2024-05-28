cbe310464e464a86367cf0f333b6e7b2514cdd10
adkshishir <adhikarishishir50@gmail.com>
2024-05-28 22:28:51 +0545

    add : auth

---
+++ src/app/(auth)/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import { Login } from '@/components/form/login';
+import React from 'react';
+
+const login = () => {
+  return (
+    <div className='flex items-center justify-center bg-gray-600 h-[100vh]'>
+    <Login />
+  </div>
+  );
+};
+
+export default login;
---
+++ src/app/(auth)/auth/register/page.tsx
@@ -0,0 +1,12 @@
+import { RegisterForm } from '@/components/form/register';
+import React from 'react';
+
+const register = () => {
+  return (
+    <div className='flex items-center justify-center bg-gray-600 h-[100vh]'>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default register;
